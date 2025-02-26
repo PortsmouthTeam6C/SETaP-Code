9d756298efb982118a5e6691ffc2cfb98cc29c38
tdgog <tdgog321@gmail.com>
2025-02-26 03:35:40 +0000

    Fix Event.java

--- Backend/src/main/java/uk/ac/port/setap/team6c/database/Event.java
+++ Backend/src/main/java/uk/ac/port/setap/team6c/database/Event.java
@@ -2,18 +2,17 @@ package uk.ac.port.setap.team6c.database;
 
 import lombok.AccessLevel;
 import lombok.Getter;
-import org.jetbrains.annotations.NotNull;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 @Getter
 public class Event {
+
     @Getter(AccessLevel.PACKAGE)
-    private int eventid;
+    private int eventId;
     @Getter(AccessLevel.PACKAGE)
     private int userid;
     private Instant StartTimestamp;
@@ -23,30 +22,33 @@ public class Event {
     private String Name;
     private String Description;
 
-    protected Event(int eventid) throws UnknownEventException {
+    protected Event(int eventId) throws UnknownEventException {
         try{
-            DatebaseManager.createconnection(connection -> {
-                PreparedStatement preparedStatement = connection.prepareStatement("select * from event where eventid = ?");
-                preparedStatement.setInt(1,eventid);
+            DatabaseManager.createConnection(connection -> {
+                PreparedStatement preparedStatement = connection.prepareStatement("select * from events where eventid = ?");
+                preparedStatement.setInt(1, eventId);
                 ResultSet resultSet = preparedStatement.executeQuery();
                 resultSet.next();
-                this.eventid = resultSet.getInt("eventid");
+                this.eventId = resultSet.getInt("eventid");
                 this.userid = resultSet.getInt("userid");
-                this.StartTimestamp = getInstant("StartTimestamp");
-                this.EndTimestamp = getInstant("EndTimestamp");
-                this.CreationTimestamp = getInstant("CreationTimestamp");
+                this.StartTimestamp = resultSet.getTimestamp("StartTimestamp").toInstant();
+                this.EndTimestamp = resultSet.getTimestamp("EndTimestamp").toInstant();
+                this.CreationTimestamp = resultSet.getTimestamp("CreationTimestamp").toInstant();
                 this.Location = resultSet.getString("Location");
                 this.Name = resultSet.getString("Name");
                 this.Description = resultSet.getString("Description");
-            })
+            });
         } catch (SQLException exception) {
             exception.printStackTrace();
             throw new UnknownEventException();
         }
     }
-    public User getCreator() throws UnknownEventException{
-        return new User(userid)
+
+    public User getCreator() throws User.UnknownUseridException {
+        return new User(userid);
     }
+
     public static class UnknownEventException extends Exception {}
+
 }
 
