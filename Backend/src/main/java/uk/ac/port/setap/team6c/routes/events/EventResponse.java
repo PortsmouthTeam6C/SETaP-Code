a11b683fbc634cfc0b8898b58e2ce964800de0ec
xjack <up2201151@myport.ac.uk>
2025-03-06 10:22:19 +0000

    finished getJoinedEvents

--- Backend/src/main/java/uk/ac/port/setap/team6c/routes/events/EventResponse.java
+++ Backend/src/main/java/uk/ac/port/setap/team6c/routes/events/EventResponse.java
@@ -2,5 +2,5 @@ package uk.ac.port.setap.team6c.routes.Events;
 
 import java.time.Instant;
 
-public record EventResponse(int eventid, int creatorid, Instant StartTimestamp, Instant EndTimeStamp, Instant CreationTimeStamp, String location, String name, String description) {
+public record EventResponse(int eventId, int userid, Instant StartTimestamp, Instant EndTimeStamp, Instant CreationTimeStamp, String location, String name, String description) {
 }
