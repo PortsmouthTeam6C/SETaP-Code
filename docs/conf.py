30d73641ced7cc53dbf072b5652a3a033bd54d8a
cyber-spirit <82119375+cyber-spirit@users.noreply.github.com>
2025-04-03 12:38:11 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,4 +3,5 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))  # Ensures ReadTheDocs can find your code
 
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions.append('myst_parser')
 html_theme = 'sphinx_rtd_theme'
